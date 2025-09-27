05b7443e1b86b82f46a5f3a64b9d8c14822ab645
francis <francis.njoku@wsu.edu>
2025-09-27 15:40:25 -0700

    threading camera frames

--- camera.cpp
+++ camera.cpp
@@ -22,8 +22,9 @@ cv::VideoCapture cap(2, cv::CAP_V4L2); // fix indexing
 using namespace cv;
 std::vector<int> count;
 
-void videoFrames(int index);
-
+void videoFrames(int index, int slots);
+std::vector<cv::Mat> frames;
+std::mutex frameMutex;
 void initialize() {
     for (size_t i = 0; i < 10; i++) {
         // Suppress OpenCV error output by redirecting stderr
@@ -49,7 +50,7 @@ int camera() {
     std::vector<std::thread> threads;
     for (size_t i = 0; i < count.size() ; i++) {
         std::cout << "Opening camera index: " << count[i] << std::endl;
-        threads.emplace_back(videoFrames, count[i]);
+        threads.emplace_back(videoFrames, count[i],i);
     }
     // Optionally, join threads if you want to wait for them (will block until all windows are closed)
     for (auto& t : threads) {
@@ -57,25 +58,33 @@ int camera() {
         std::cout << "Thread ID: " << std::this_thread::get_id() << std::endl;
     }
     return 0;
+    
 }
 
 
-void videoFrames(int index){
+void videoFrames(int index, int slot){
       cv::VideoCapture cap(index, cv::CAP_ANY);
       std::string windowName = "MJPEG Camera " + std::to_string(index);  ///window names depending on how many fr
-    Mat frames; 
+ // frames to smal size of 8 bits
+Mat frame ;
      // Set MJPEG mode explicitly (fourcc 'MJPG')
     cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M','J','P','G'));
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
     cap.set(cv::CAP_PROP_FPS, 120); 
     while(true){
-    cap >> frames;
+    cap >> frame;
     if (frames.empty() ) {
         std::cerr << "Camera not opened.\n";
         break;
     }else 
+    {
 
+          {
+            std::lock_guard<std::mutex> lock(frameMutex);
+            frame.copyTo(frames[slot]);  // update shared slot
+        }
+    }
         cv::imshow(windowName, frames);
         if (cv::waitKey(1) == 'q') break;
 
